c065fa2200fe2e4aa39b42e5ac9a7fa916113150
glebza <glebza@mail.ru>
2023-02-09 10:03:03 +0300

    changed strategy mechanism

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
-https://www.dataquest.io/blog/python-datetime-tutorial/
+структура модуля стратегия
+
+на вход подаются свечки с закрывающими ценами и наличие позиции
+
+на выходе дают команду WAIT,BUY,SELL и цену 
 
 
-Process
+ variants:
+ 
+in position:
+ wait or sell 
 
-check open deal and in_position flag
-1) if it is then get order_id
-check the sell conditions: momentum > 70 
-   
-2)if it isn't then try to get deal from the db
-if it is then in_position = true and go to 1)
-if it isn't then check buy conditions
+not in position:
+ wait or buy
 
-buy, save order id into db and in_position = true
-
+graph TD
+    A[Enter Chart Definition] --> B(Preview)
+    B --> C{decide}
+    C --> D[Keep]
+    C --> E[Edit Definition]
+    E --> B
+    D --> F[Save Image and Code]
+    F --> B
 
 
--- glebza/tradeapp/src/strategies/boll_macd_rsi.py
+++ glebza/tradeapp/src/strategies/boll_macd_rsi.py
@@ -2,22 +2,27 @@ import talib
 import numpy
 
 PROFIT_RATE = 8
+LOSE_RATE = -10
 RSI_OVERSOLD = 51
 RSI_PERIOD = 15
 
 
-def process(close_prices):
-    track = 'WAIT', 0, 0, 0
-    np_closes = numpy.array(close_prices)
+def process(close_prices, order):
+    track = dict({'action': 'WAIT', 'price': 0})
+    np_closes = numpy.append(numpy.array(close_prices),close_prices[-1])
     rsi = talib.RSI(np_closes, RSI_PERIOD)
     macd, signal, macd_diff = talib.MACD(np_closes, fastperiod=12, slowperiod=26, signalperiod=9)
 
-    if rsi[-2] < RSI_OVERSOLD \
-            and macd[-1] > signal[-1] and macd[-2] <= signal[-2] and macd[-1] < 0:
-        close_price = close_prices[-1]
-        buy_price = close_price + 0.1
-        sell_price = buy_price + PROFIT_RATE
-        lose_price = close_price - 5 * PROFIT_RATE
-        track = 'BUY', buy_price, sell_price, lose_price
+    if order:
+        if (float(close_prices[-1]) - float(order['price'])) >= PROFIT_RATE \
+                or (macd[-1] < signal[-1] and macd[-2] >= signal[-2]):
+            track = dict({'action': 'SELL', 'price': close_prices[-1]})
+    else:
+        if rsi[-1] < RSI_OVERSOLD and macd[-1] > signal[-1] and macd[-2] <= signal[-2] and macd[-1] < 0:
+            close_price = close_prices[-1]
+            buy_price = close_price + 0.1
+            track = dict({'action': 'BUY', 'price': buy_price})
 
     return track
+
+#   or (float(close_prices[-1]) - float(order['price'])) <= LOSE_RATE \
